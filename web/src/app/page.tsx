2d97460e883e0657a75029e929a29fad6e39be78
Davi Snaider Mendoza <davisnaider06@gmail.com>
2025-12-02 15:46:14 -0300

    Fix: Adicionando use client na pagina de registro

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
